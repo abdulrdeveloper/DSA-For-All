e9cc2b97d6e557776d013927bba60f994474722b
Abdul Rahman <abdulrahman2040b@gmail.com>
2026-08-11 21:21:00 +0500

    Refactor operators.cpp to uncomment operator examples and add type_casting.cpp with implicit and explicit type casting examples

--- Learn C++/operators.cpp
+++ Learn C++/operators.cpp
@@ -5,33 +5,33 @@ int main() {
     int a = 10;
     int b = 20;
 
-    // // increment and decrement operators
-    // cout << "a++ = " << a++ << endl; // Post-increment
-    // cout << "++a = " << ++a << endl; // Pre-increment
-    // cout << "b++ = " << b++ << endl; // Post-increment
-    // cout << "++b = " << ++b << endl; // Pre-increment
+    // increment and decrement operators
+    cout << "a++ = " << a++ << endl; // Post-increment
+    cout << "++a = " << ++a << endl; // Pre-increment
+    cout << "b++ = " << b++ << endl; // Post-increment
+    cout << "++b = " << ++b << endl; // Pre-increment
 
-    // // Arithmetic Operators
-    // cout << "a + b = " << a + b << endl; // Addition
-    // cout << "a - b = " << a - b << endl; // Subtraction
-    // cout << "a * b = " << a * b << endl; // Multiplication
-    // cout << "a / b = " << a / b << endl; // Division
-    // cout << "a % b = " << a % b << endl; // Modulus
+    // Arithmetic Operators
+    cout << "a + b = " << a + b << endl; // Addition
+    cout << "a - b = " << a - b << endl; // Subtraction
+    cout << "a * b = " << a * b << endl; // Multiplication
+    cout << "a / b = " << a / b << endl; // Division
+    cout << "a % b = " << a % b << endl; // Modulus
 
-    // // Relational Operators
-    // cout << "a == b: " << (a == b) << endl; // Equal to
-    // cout << "a != b: " << (a != b) << endl; // Not equal to
-    // cout << "a > b: " << (a > b) << endl;   // Greater than
-    // cout << "a < b: " << (a < b) << endl;   // Less than
-    // cout << "a >= b: " << (a >= b) << endl; // Greater than or equal to
-    // cout << "a <= b: " << (a <= b) << endl; // Less than or equal to
+    // Relational Operators
+    cout << "a == b: " << (a == b) << endl; // Equal to
+    cout << "a != b: " << (a != b) << endl; // Not equal to
+    cout << "a > b: " << (a > b) << endl;   // Greater than
+    cout << "a < b: " << (a < b) << endl;   // Less than
+    cout << "a >= b: " << (a >= b) << endl; // Greater than or equal to
+    cout << "a <= b: " << (a <= b) << endl; // Less than or equal to
 
-    // // Logical Operators
-    // bool x = true;
-    // bool y = false;
-    // cout << "x && y: " << (x && y) << endl; // Logical AND
-    // cout << "x || y: " << (x || y) << endl; // Logical OR
-    // cout << "!x: " << (!x) << endl;         // Logical NOT
+    // Logical Operators
+    bool x = true;
+    bool y = false;
+    cout << "x && y: " << (x && y) << endl; // Logical AND
+    cout << "x || y: " << (x || y) << endl; // Logical OR
+    cout << "!x: " << (!x) << endl;         // Logical NOT
 
     // Bitwise Operators
     cout << "a & b = " << (a & b) << endl; // Bitwise AND

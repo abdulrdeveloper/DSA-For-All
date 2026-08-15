372945c21a4d8ab192b8f445c14b716cb3aebad3
Abdul Rahman <abdulrahman2040b@gmail.com>
2026-08-15 21:06:55 +0500

    Remove unused best-time-to-buy-sell-stock.ts file

--- arrays/best-time-to-buy-sell-stock.ts
+++
@@ -1,8 +0,0 @@
-// Day 1 - Best Time to Buy and Sell Stock
-
-function addNumbers(a: number, b: number): number {
-  return a + b;
-}
-
-console.log(addNumbers(51, 11));
-console.log(addNumbers(5, 11));

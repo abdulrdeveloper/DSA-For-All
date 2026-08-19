6e18a68b9d623aea84432fe5498da270cded254a
Abdul Rahman <abdulrahman2040b@gmail.com>
2026-08-19 10:51:30 +0500

    Initial snapshot

---
+++ typescript/arrays/best-time-to-buy-sell-stock.ts
@@ -0,0 +1,12 @@
+// Day 1 - Best Time to Buy and Sell Stock
+
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+const num:number=23;
+const num4:number=45;
+
+console.log(addNumbers(num, num4));
+
+//test cases successfully passed for the addNumbers function. Typescript is fully working .
